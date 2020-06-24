d540748bf5db6dfbe1b3ea719e9b83929c5a8ce6
leandropinheiroo <leandropinheiro98@gmail.com>
2020-06-24 16:03:22 -0300

    Created Header component and remove default css from main page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
-}
+import Header from './components/Header';
+
+import './styles.css';
+
+const App = () => (
+  <div className="App">
+    <Header />
+  </div>
+);
+
 
 export default App;
